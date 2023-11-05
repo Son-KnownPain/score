799b5826cee2772599c113be4b070efe49dbcd75
Son-KnownPain <sonhong443@gmail.com>
2023-11-05 15:36:21 +0700

    new

--- assets/js/testupgrade.js
+++ assets/js/testupgrade.js
@@ -1,13 +1,39 @@
+var sc = 0;
+var fail = 0;
+
 function upgrade() {
     const ratioElm = document.getElementById('ratio');
 
     const ratioVal = ratioElm.value;
 
     var randomNumber = Math.floor(Math.random() * 100);
-    console.log(randomNumber);
+    const resultElm = document.getElementById('box');
     if (randomNumber < ratioVal) {
-        document.getElementById('display-result').textContent = 'Thành công!';
+        scFn();
+        resultElm.innerHTML = '<h1 style="color: green;" id="display-result">Thành công!</h1>';
     } else {
-        document.getElementById('display-result').textContent = 'Thất bại!';
+        failFn();
+        resultElm.innerHTML = '<h1 style="color: red;" id="display-result">Thất bại!</h1>';
     }
+}
+
+function scFn() {
+    sc++;
+    const resultElm = document.getElementById('sc');
+    resultElm.textContent = sc;
+}
+
+function failFn() {
+    fail++;
+    const resultElm = document.getElementById('fail');
+    resultElm.textContent = fail;
+}
+
+function rsFn() {
+    sc = 0;
+    fail = 0;
+    const resultElm1 = document.getElementById('sc');
+    resultElm1.textContent = sc;
+    const resultElm2 = document.getElementById('fail');
+    resultElm2.textContent = fail;
 }
